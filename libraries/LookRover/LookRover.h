c375e2b8a1cad5224d0ac1c4f43ba929507e23b5
unknown <501810746@BMSRM43PC25.bms.venturausd.k12.ca.us>
2015-05-08 14:40:37 -0700

    This is my first commit of Look Rover.

--- libraries/LookRover/LookRover.h
+++ libraries/LookRover/LookRover.h
@@ -1,1 +1,18 @@
+#ifndef LookRover_h
+#define LookRover_h
 
+#include "Arduino.h"
+#includ <Servo.h>
+#include <NewPing.h>
+#include <RangeRover.h>
+
+class LookRover : public RangeRover
+{
+ protected:
+	   Servo	_faceServo;
+
+ public:
+		LookRover();
+		void init(	int servoLeftPin,
+				int servoLeftStop,
+				int servo
