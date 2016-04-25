671849cdb532986e51c57414267b63b867b03a98
Greg <gregory.yenney@gmail.com>
2016-04-24 19:23:38 -0700

    Update SMS_Light_Sensor.ino

--- SMS_Light_Sensor/SMS_Light_Sensor.ino
+++ SMS_Light_Sensor/SMS_Light_Sensor.ino
@@ -157,7 +157,7 @@ void SendOnMessage()
   Serial.println("SendMessage()");
   mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
   delay(1000);  // Delay of 1000 milli seconds or 1 second
-  mySerial.println("AT+CMGS=\"8057270090\"\r"); // Replace x with mobile number
+  mySerial.println("AT+CMGS=\"your_phone_number\"\r"); // Replace x with mobile number
   delay(500);
   mySerial.println("Light was turned on!");// The SMS text you want to send
   delay(500);
@@ -171,7 +171,7 @@ void SendOffMessage()
   Serial.println("SendMessage()");
   mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
   delay(1000);  // Delay of 1000 milli seconds or 1 second
-  mySerial.println("AT+CMGS=\"8057270090\"\r"); // Replace x with mobile number
+  mySerial.println("AT+CMGS=\"your_phone_number\"\r"); // Replace x with mobile number
   delay(500);
   mySerial.println("Light was turned off!");// The SMS text you want to send
   delay(500);
